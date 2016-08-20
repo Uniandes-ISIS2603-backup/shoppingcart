eb2923ec77810a8e201b61a1bcc78ad08de6c7b7
rcasalla <rcasalla@uniandes.edu.co>
2016-08-20 11:12:18 -0500

    Controlador completo

--- src/main/webapp/src/app.js
+++ src/main/webapp/src/app.js
@@ -1,11 +1,32 @@
 var app = angular.module('MainApp', []);
 
-app.controller('shopController', function ($scope) {
-    $scope.invoice = {
-        items: [{
-                qty: 10,
-                description: 'item',
-                cost: 9.95
-            }]
+app.controller('shopController', function($scope) {
+  $scope.invoice = {
+      items: [{
+        qty: 10,
+        description: 'item',
+        cost: 9.95
+      }]
+    },
+    $scope.addItem = function() {
+      $scope.invoice.items.push({
+        qty: 1,
+        description: '',
+        cost: 0
+      });
+    },
+    $scope.deleteItem = function(index) {
+      $scope.invoice.items.splice(index, 1);
+    },
+    $scope.subTotal = function(item) {
+      return item.qty * item.cost;
+    },
+    $scope.total = function() {
+      var total = 0;
+      angular.forEach($scope.invoice.items, function(item) {
+        total += item.qty * item.cost;
+      });
+
+      return total;
     }
 });
